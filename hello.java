658286a6ab5d47950f7e49d8490dc8a43e186918
niabinugeorge <niabinugeorge@gmail.com>
2026-03-10 19:06:55 +0530

    UC2: Display Hello with command line argument

--- hello.java
+++ hello.java
@@ -1,7 +1,11 @@
 public class hello {
     public static void main(String[] args) {
+    if(args.length > 0){
         String name = args[0];
         System.out.println("Hello, " + name + "!");
+    } else {
+        System.out.println("Hello, World!");
+    }
         
     }
 }
